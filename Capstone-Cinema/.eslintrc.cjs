c40916611fbc50226980b2bf107f9c027bb717c5
Hien <hientnt0999@gmail.com>
2023-11-20 13:45:49 +0700

    .

--- Capstone-Cinema/.eslintrc.cjs
+++ Capstone-Cinema/.eslintrc.cjs
@@ -1,15 +1,20 @@
+
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
   ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  plugins: ["react-refresh"],
-  ignorePatterns: ["*/typescript/.ts"],
+  paserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  ignorePatterns: ['**/typescript/*.ts'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
   rules: {
-    "react-refresh/only-export-components": "warn",
+    'react-refresh/only-export-components': 'warn'
+
   },
-};
+}
+
+
+ 
